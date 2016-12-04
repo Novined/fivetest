b612ca808d49524872c9b8e660dbef0b2a510a94
Denis <denis@localhost.localdomain>
2016-12-04 17:40:11 +0300

    oshi

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var db = require('./utils/database.js');
 var sync = require('./utils/sync.js');
 
 const app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3002;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 db.setUpConnection();
