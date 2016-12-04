8a192ed15ce59782b1803e8cd008b0947d610632
Denis <denis@localhost.localdomain>
2016-12-04 17:09:44 +0300

    openshift changes

--- utils/database.js
+++ utils/database.js
@@ -13,11 +13,16 @@ require('../models/Word');
 
 const Article = mongoose.model('Article');
 const Word = mongoose.model('Word');
-var db_name = 'articles';
-var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
-
-if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+var db_name = 'fivetest';
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/' + db_name;
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
 }
 
 function setUpConnection() {
