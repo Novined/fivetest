24c9b2324813e85dabc88e42a000c9a746cce933
Denis <denis@localhost.localdomain>
2016-12-05 19:29:21 +0300

    my changes

--- utils/api.js
+++ utils/api.js
@@ -2,11 +2,13 @@ exports.listArticles = listArticles;
 exports.listWords = listWords;
 
 var axios = require('axios');
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP;
+var port    = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
 
 function listArticles() {
-    return axios.get('http://localhost:8080/api/articles');
+    return axios.get('http://' + ipaddr + ':' + port + '/api/articles');
 }
 
 function listWords() {
-    return axios.get('http://localhost:8080/api/words');
+    return axios.get('http://' + ipaddr + ':' + port + '/api/words');
 }
